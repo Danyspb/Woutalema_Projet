23b1e390896416f312eaed4a5c7f8547d8890976
Stanislas Pierre Bombolong <danyspb4ever@gmail.com>
2021-07-28 15:04:09 +0000

    authentification des utilisateurs

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,10 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,25 +18,29 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class,[
+                'label' =>'Login'
+            ])
             ->add('roles', CollectionType::class,[
                 'entry_type' => ChoiceType::class,
                 'entry_options' =>[
                     'label' => false,
                     'choices' =>[
-                        'Admin'=>'ROLE_ADMIN',
+                        ''=>'',
                         'Client' => 'ROLE_CLIENT',
                         'Prestataire' =>'ROLE_PRESTATAIRE',
                         'Livreur' =>'ROLE_LIVREUR'
                     ]
                 ]
             ])
-            ->add('password')
+            ->add('password',PasswordType::class)
             ->add('nom')
             ->add('prenom')
-            ->add('date_naissance')
+            ->add('date_naissance', DateType::class,[
+                'widget' => 'single_text'
+            ])
             ->add('telephone')
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('numero_cni')
             ->add('adresse')
         ;
